0e3f27e821d24700ea46ce4281e9413223bb9833
smeil123 <dmswl3976@naver.com>
2018-02-12 22:31:40 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+	# post는 쿼리셋의 이름 
+	posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+	#posts = 'test massage'
+	return render(request, 'blog/post_list.html',{'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
